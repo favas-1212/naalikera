6609c993aa109110ff3c51b948ab8216a1076a57
favas1f <favasmhd2003@gmail.com>
2025-10-19 12:52:16 +0530

    initial Django project commit

---
+++ naalikera/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for naalikera project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naalikera.settings')
+
+application = get_asgi_application()
